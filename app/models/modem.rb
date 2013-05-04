b1f952744d8094bdc4f38283757f9f2ebc0d4b91
Mitsuoka <mitsuoka@ubuntu.ubuntu-domain>
2013-05-04 08:36:57 -0300

    sleep a week for listning activity

--- app/models/modem.rb
+++ app/models/modem.rb
@@ -20,31 +20,39 @@ class Modem
 
     	Thread.new{
     		Thread.list.each {|a| puts " 1 -#{a.inspect}: #{a[:name]}"}
+            count = 7
 
-    		while (Time.now.hour != 18 )
-    			sleep(3600) #magic day number
-    			puts"hey"
-    		end
+            while(1)
+        		while (Time.now.hour != 12 )
+        			sleep(3600) #magic day number
+        			puts"hey"
+        		end
 
-    		Bill.all.each  do |a| 
-                puts "#{a.date.day}"
-    			if (Time.now.day == a.date.day)
-    				user = User.where(:id => a.user_id).first 
-    				name_bill = a.name
-                    sms = "Amanha pagar sua conta de #{name_bill}."
-                    puts "#{sms}"
-    				$GSM.send_sms!(user.phone_number,sms)
-    				a.date = a.date.next_month
-    			    puts "#{a.date}"
+        		Bill.all.each  do |a| 
+                    puts "#{a.date.day}"
+        			if (Time.now.day == a.date.day)
+        				user = User.where(:id => a.user_id).first 
+        				name_bill = a.name
+                        sms = "Amanha pagar sua conta de #{name_bill}."
+                        puts "#{sms}"
+        				$GSM.send_sms!(user.phone_number,sms)
+        				a.date = a.date.next_month
+        			    puts "#{a.date}"
+                    end
+        		end
+
+                if((count%7) == 0 ) #just do that once a week
+                    Dream.all.each do |d|
+                        days = Time.now - d.next_week
+                        if(days >= 604800)
+                            user = User.where(:id => d.user_id).first 
+                            sms = "Vamos la, faz tempo que nao economiza nada!"
+                            $GSM.send_sms!(user.phone_number,sms)
+                        end
+                    end
                 end
-    		end
-            Dream.all.each do |d|
-                days = Time.now - d.next_week
-                if(days >= 604800)
-                    user = User.where(:id => d.user_id).first 
-                    sms = "Vamos la, faz tempo que nao economiza nada!"
-                    $GSM.send_sms!(user.phone_number,sms)
-                end
+                sleep(3600*20) #wait 20h to listening again
+                count +=1
             end
     	}
     end
